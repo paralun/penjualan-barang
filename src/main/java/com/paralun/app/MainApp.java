04b0ea73a0be1f0412d4605a3e1aa5afaea438a6
James Kusmambang <kusmambang@gmail.com>
2016-12-10 20:08:02 +0700

    create projects

---
+++ src/main/java/com/paralun/app/MainApp.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2016 | James Kusmambang
+ * Source : https://github.com/paralun
+ */
+package com.paralun.app;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
