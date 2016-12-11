ad42b0e29ecf9f5574e4a7b833b402fae1ffebbb
James Kusmambang <kusmambang@gmail.com>
2016-12-11 12:48:39 +0700

    create model master

---
+++ src/main/java/com/paralun/app/model/Barang.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2016 | James Kusmambang
+ * Source : https://github.com/paralun
+ */
+package com.paralun.app.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "t_barang")
+public class Barang implements Serializable{
+    
+    @Id
+    @Column(name = "kode_brg", nullable = false, length = 6)
+    private String kodeBrg;
+    @Column(name = "nama_Brg", length = 50)
+    private String namaBrg;
+    @Column(name = "satusn_brg", length = 10)
+    private String satuanBrg;
+    @Column(name = "harga_brg")
+    private Integer hargaBrg;
+    @Column(name = "stok_brg")
+    private Integer stokBrg;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "create_date")
+    private Date createDate;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "update_date")
+    private Date updateDate;
+
+    public Barang() {
+    }
+
+    public Barang(String kodeBrg) {
+        this.kodeBrg = kodeBrg;
+    }
+
+    public String getKodeBrg() {
+        return kodeBrg;
+    }
+
+    public void setKodeBrg(String kodeBrg) {
+        this.kodeBrg = kodeBrg;
+    }
+
+    public String getNamaBrg() {
+        return namaBrg;
+    }
+
+    public void setNamaBrg(String namaBrg) {
+        this.namaBrg = namaBrg;
+    }
+
+    public String getSatuanBrg() {
+        return satuanBrg;
+    }
+
+    public void setSatuanBrg(String satuanBrg) {
+        this.satuanBrg = satuanBrg;
+    }
+
+    public Integer getHargaBrg() {
+        return hargaBrg;
+    }
+
+    public void setHargaBrg(Integer hargaBrg) {
+        this.hargaBrg = hargaBrg;
+    }
+
+    public Integer getStokBrg() {
+        return stokBrg;
+    }
+
+    public void setStokBrg(Integer stokBrg) {
+        this.stokBrg = stokBrg;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+}
---
+++ src/main/java/com/paralun/app/model/Pelanggan.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2016 | James Kusmambang
+ * Source : https://github.com/paralun
+ */
+package com.paralun.app.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "t_pelanggan")
+public class Pelanggan implements Serializable {
+    
+    @Id
+    @Column(name = "kode_plg", nullable = false, length = 6)
+    private String kodePlg;
+    @Column(name = "nama_plg", length = 50)
+    private String namaPlg;
+    @Column(name = "alamat_plg", length = 100)
+    private String alamatPlg;
+    @Column(name = "tlp_plg", length = 12)
+    private String teleponPlg;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "create_date")
+    private Date createDate;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "update_date")
+    private Date updateDate;
+
+    public Pelanggan() {
+    }
+
+    public String getKodePlg() {
+        return kodePlg;
+    }
+
+    public void setKodePlg(String kodePlg) {
+        this.kodePlg = kodePlg;
+    }
+
+    public String getNamaPlg() {
+        return namaPlg;
+    }
+
+    public void setNamaPlg(String namaPlg) {
+        this.namaPlg = namaPlg;
+    }
+
+    public String getAlamatPlg() {
+        return alamatPlg;
+    }
+
+    public void setAlamatPlg(String alamatPlg) {
+        this.alamatPlg = alamatPlg;
+    }
+
+    public String getTeleponPlg() {
+        return teleponPlg;
+    }
+
+    public void setTeleponPlg(String teleponPlg) {
+        this.teleponPlg = teleponPlg;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+    
+}
