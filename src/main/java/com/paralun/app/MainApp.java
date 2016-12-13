820510b32920715c2d341c9a8ba29ffbae868484
James Kusmambang <kusmambang@gmail.com>
2016-12-13 22:18:33 +0700

    create view sample

--- src/main/java/com/paralun/app/MainApp.java
+++ src/main/java/com/paralun/app/MainApp.java
@@ -4,6 +4,7 @@
  */
 package com.paralun.app;
 
+import com.paralun.app.model.Barang;
 import com.paralun.app.model.Penjualan;
 import com.paralun.app.model.PenjualanDetail;
 import com.paralun.app.service.MasterService;
@@ -19,21 +20,5 @@ public class MainApp {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-beans.xml");
         MasterService service = (MasterService) ctx.getBean("masterService");
         PenjualanService ps = (PenjualanService) ctx.getBean("penjualanService");
-        
-        Penjualan p = ps.getPenjualan("J001");
-        System.out.println(p.getNoJual());
-        System.out.println(p.getPelanggan().getNamaPlg());
-        System.out.println(p.getTotalJual());
-        System.out.println(p.getTglJual());
-        System.out.println("======================");
-        
-        List<PenjualanDetail> details = p.getPenjualanDetails();
-        for(PenjualanDetail d : details) {
-            System.out.println(d.getNoJual().getNoJual());
-            System.out.println(d.getBarang().getNamaBrg());
-            System.out.println(d.getJmlJual());
-            System.out.println(d.getHargaJual());
-        } 
     }
-    
 }
